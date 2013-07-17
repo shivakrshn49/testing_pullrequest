b83597b68b8daf68af7f1ca83d0b038acb03277d
shivakrishnab <shiva@agiliq.com>
2013-07-17 16:22:52 +0530

    Added empty lines at the end

--- example.py
+++ example.py
@@ -6,3 +6,5 @@ print "Today is {0}".format(str(datetime.today()))
 print "Time is {0}".format(str(time.now()))
 
 print "Testing regorously for pulling request"
+
+print "----------------------------------"
